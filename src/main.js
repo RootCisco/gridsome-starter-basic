77757bae8da99ad2de6d84ee93995ba18d889ce2
cisco_yoshikawa <exiria7@gmail.com>
2021-04-13 18:18:48 +0900

    viewport devでおかしくなるのでindex.htmlに寄住する方式に

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import DefaultLayout from '~/layouts/Default.vue';
 export default function (Vue, { router, head, isClient }) {
   // head settings
   head.htmlAttrs.lang = 'ja';
-  head.meta.find((el) => el.key === 'viewport').content = 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0';
 
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
